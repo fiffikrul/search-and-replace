34a9f6f26d2a07c18c4eeac94e485f6f6caa8568
fiffikrul <filllip1996@gmail.com>
2020-09-20 02:05:07 +0200

    adjust css

--- src/components/Styles/Colors.ts
+++ src/components/Styles/Colors.ts
@@ -1,4 +1,4 @@
 export const inputGrey = "#2c2d30";
 export const backgroundGrey = "#202124";
 export const textGrey = "#B1b1b1";
-export const selectBlue = "#344b63";
+export const selectBlue = "#4397e0";
--- src/components/Styles/StyledInputField.ts
+++ src/components/Styles/StyledInputField.ts
@@ -2,15 +2,15 @@ import styled from "styled-components";
 import { inputGrey, selectBlue, textGrey } from "./Colors";
 
 export const StyledInputField = styled.input`
+  height: auto;
   width: auto;
-  height: 1rem;
-  padding: 0.5rem 0.5rem;
+  padding: 0.2rem 0.5rem;
   background: ${inputGrey};
   color: ${textGrey};
-  font-size: 1rem;
-  font-weight: 500;
-  border: transparent;
+  border: none;
   outline: none;
+  font-size: 0.8rem;
+  font-weight: 500;
 
   :focus {
       outline: 0.1rem solid ${selectBlue};
